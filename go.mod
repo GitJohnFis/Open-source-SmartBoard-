08dea81d509140bd8daedf874d1d3249f87748a1
GitJohnFis <jfisiiahi69@gmail.com>
2025-05-04 23:27:26 +0000

    update with catch feature and pokemon types and names

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/GitJohnFis/Open-source-SmartBoard-
-
-go 1.24.1
--- wagslane/main.go
+++ wagslane/main.go
@@ -8,9 +8,10 @@ import (
 
 
 func main() {
-	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
-cfg := &config{
-	pokeapiClient: pokeClient,
+	pokeClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
+	cfg := &config{
+		caughtPokemon: map[string]pokeapi.Pokemon{},
+		pokeapiClient: pokeClient,
 }
 
 
